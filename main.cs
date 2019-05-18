148059614c47a24d0b2c9c4283485f49c8aed071
Chunhui Liu <37141993+ChunhuiL@users.noreply.github.com>
2019-05-18 20:53:26 +0200

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,18 +1,18 @@
-LDI R21, (1 << PE5)
-	OUT DDRE,R21
-	LDI R22,0xFF
-	OUT DDRB,R22
+LDI R21, (1 << PE4)          ;set the register
+	OUT DDRE,R21         ;set port E to output
+	
 hel:
-	IN R20,PINE
-	ANDI R20, (1 << PE4)
-	CPI R20,0
-	BRNE on
+	IN R20,PINE          ;set the register
+	ANDI R20, (1 << PE5) ;do and
+	CPI R20,0            ;compare R20 and 0
+	BRNE on              ;if not,go to on
 	LDI R17,0
 	OUT PORTE,R17
 	JMP hel
 on:
-	LDI R17, (1 << PE5)
+	LDI R17, (1 << PE4)  ;set the register
 	OUT PORTE,R17
 	JMP hel
+
 
 
